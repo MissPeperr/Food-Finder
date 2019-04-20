31b244bc05ce1695452a397bd53834a934a9517b
Madi Peper <madisonpeper@gmail.com>
2019-04-20 10:06:50 -0500

    added a navbar with no functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './components/navbar/navbar.js';
 import logo from './logo.svg';
 import './App.scss';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
